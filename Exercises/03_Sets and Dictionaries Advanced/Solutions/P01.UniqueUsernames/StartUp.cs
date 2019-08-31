2348ee95009f60ee4ae102b0581b89a9799ab809
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-08-31 19:34:04 +0300

    added Sets and Dictionaries Advanced

---
+++ Exercises/03_Sets and Dictionaries Advanced/Solutions/P01.UniqueUsernames/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P01.UniqueUsernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int countOfUsernames = int.Parse(Console.ReadLine());
+
+            HashSet<string> users = new HashSet<string>();
+
+            for (int user = 1; user <= countOfUsernames; user++)
+            {
+                string userName = Console.ReadLine();
+
+                users.Add(userName);
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ Exercises/03_Sets and Dictionaries Advanced/Solutions/P02.SetsOfElements/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace P02.SetsOfElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sizeFirstSet = sizes[0];
+            int sizeSecondSet = sizes[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < sizeFirstSet; i++)
+            {
+                int currentInput = int.Parse(Console.ReadLine());
+                firstSet.Add(currentInput);
+            }
+
+            for (int i = 0; i < sizeSecondSet; i++)
+            {
+                int currentInput = int.Parse(Console.ReadLine());
+                secondSet.Add(currentInput);
+            }
+
+            foreach (var currentNumber in firstSet)
+            {
+                if (secondSet.Contains(currentNumber))
+                {
+                    Console.Write("{0} ", currentNumber);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
