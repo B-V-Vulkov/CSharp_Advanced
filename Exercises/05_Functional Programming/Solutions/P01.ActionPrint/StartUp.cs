e772493723be157938786dd42df7702c8e458945
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-02 17:43:54 +0300

    Add P01. Action Print

---
+++ Exercises/05_Functional Programming/Solutions/P01.ActionPrint/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.ActionPrint
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Action<string[]> PrintNames = names =>
+                Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            string[] inputNames = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            PrintNames(inputNames);
+        }
+    }
+}
