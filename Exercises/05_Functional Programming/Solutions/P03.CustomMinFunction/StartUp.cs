e3bf1fd0abb3e7c6f5c004d0d0a9c5aae67ebdca
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-02 18:05:37 +0300

    Add P03. Custom Min Function

---
+++ Exercises/05_Functional Programming/Solutions/P03.CustomMinFunction/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace P03.CustomMinFunction
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Func<int[], int> smallestNumber = numbers =>
+            {
+                int minValue = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (number < minValue)
+                    {
+                        minValue = number;
+                    }
+                }
+
+                return minValue;
+            };
+
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(smallestNumber(inputNumbers));
+
+        }
+    }
+}
