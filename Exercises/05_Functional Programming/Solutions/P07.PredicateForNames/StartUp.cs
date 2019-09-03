4bbff5d4dd69e94b99c2e9b3f291e2de6952c35c
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-03 11:50:12 +0300

    Add P07. Predicate For Names

---
+++ Exercises/05_Functional Programming/Solutions/P07.PredicateForNames/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace P07.PredicateForNames
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int lessNumber = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> isLess = name => name.Length <= lessNumber;
+
+            names = names.Where(name => isLess(name)).ToArray();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
