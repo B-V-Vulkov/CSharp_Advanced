6c4fa4abc7a093fd61f18f35d3566684e8a189de
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-03 12:09:18 +0300

    Add P08. Custom Comparator

---
+++ Exercises/05_Functional Programming/Solutions/P08.CustomComparator/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace P08.CustomComparator
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(inputNumbers);
+
+            Predicate<int> isEven = number => number % 2 == 0;
+            Predicate<int> isOdd = number => number % 2 != 0;
+
+            Console.Write(string.Join(" ", inputNumbers.Where(n => isEven(n))) + " ");
+            Console.WriteLine(string.Join(" ", inputNumbers.Where(n => isOdd(n))));
+
+
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+
+
+
+        }
+    }
+}
