7519c60d2d92b6dff0709c62037dc10084f06094
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-03 18:57:15 +0300

    Add P01. Define a Class Person

---
+++ Exercises/06_Defining Classes/Solutions/P01.DefineClassPerson/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Exercises/06_Defining Classes/Solutions/P01.DefineClassPerson/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person firstPerson = new Person();
+            Person secontPerson = new Person();
+            Person thirdPerson = new Person();
+
+            firstPerson.Name = "Pesho";
+            firstPerson.Age = 20;
+
+            secontPerson.Name = "Gosho";
+            secontPerson.Age = 18;
+
+            thirdPerson.Name = "Stamat";
+            thirdPerson.Age = 43;
+        }
+    }
+}
