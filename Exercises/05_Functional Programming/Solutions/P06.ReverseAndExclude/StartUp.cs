14278076a88dec17dcaa435d4f56ce623735c3a9
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-03 11:38:29 +0300

    Add P06. Reverse And Exclude

---
+++ Exercises/05_Functional Programming/Solutions/P06.ReverseAndExclude/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace P06.ReverseAndExclude
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int divisibleNumber = int.Parse(Console.ReadLine());
+
+            Predicate<int> isNotDivisible = number => number % divisibleNumber != 0;
+
+            inputNumbers = inputNumbers
+                .Where(n => isNotDivisible(n))
+                .ToArray();
+
+            inputNumbers = inputNumbers.Reverse().ToArray();
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+        }
+    }
+}
