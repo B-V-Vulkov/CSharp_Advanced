fdb9c4e59ed64d5ae3d391729704b7c9fde237ef
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-03 15:22:58 +0300

    Add P12. TriFunction

---
+++ Exercises/05_Functional Programming/Solutions/P12.TriFunction/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace P12.TriFunction
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isLarger = (name, charsLarger) =>
+                name.Sum(x => x) >= charsLarger;
+
+            Func<string[], Func<string, int, bool>, string> nameFilter =
+                (inputNames, isLargerFilter) => 
+                inputNames.FirstOrDefault(x => isLargerFilter(x, length));
+
+            string resultName = nameFilter(names, isLarger);
+            Console.WriteLine(resultName);
+        }
+    }
+}
