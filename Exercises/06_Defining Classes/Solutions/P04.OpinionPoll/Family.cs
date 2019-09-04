df2956c6dffccddc1ebfdce6ef8798ac23246d0a
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-04 14:56:40 +0300

    Add P04. Opinion Poll

---
+++ Exercises/06_Defining Classes/Solutions/P04.OpinionPoll/Family.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Family
+    {
+        private List<Person> familyMember;
+
+        public Family()
+        {
+            familyMember = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            familyMember.Add(person);
+        }
+
+        public List<Person> GetOldMembers(int years)
+        {
+            List<Person> oldMembers = familyMember
+                .Where(x => x.Age > years)
+                .ToList();
+
+            return oldMembers;
+        }
+    }
+}
---
+++ Exercises/06_Defining Classes/Solutions/P04.OpinionPoll/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 1; i <= countOfPeople; i++)
+            {
+                string[] peopleInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string currentName = peopleInfo[0];
+                int currentAge = int.Parse(peopleInfo[1]);
+
+                Person currentPerson = new Person(currentName, currentAge);
+
+                family.AddMember(currentPerson);
+            }
+
+            List<Person> oldMembers = family.GetOldMembers(30);
+            oldMembers = oldMembers
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            foreach (var person in oldMembers)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
