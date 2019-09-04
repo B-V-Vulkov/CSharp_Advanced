4572e2572025f60cc9dc93a3c426ffb44f1f9a74
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-04 15:34:56 +0300

    Add P05. Date Modifier

---
+++ Exercises/06_Defining Classes/Solutions/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,41 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DateModifier
+    {
+        private string firstDate;
+        private string secondDate;
+
+        public DateModifier(string firstDay, string secondDay)
+        {
+            this.firstDate = firstDay;
+            this.secondDate = secondDay;
+        }
+
+        public string DaysBetween()
+        {
+            string[] splittedFirstDate = firstDate
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] splittedSecondDate = secondDate
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int firstYear = int.Parse(splittedFirstDate[0]);
+            int firstMonth = int.Parse(splittedFirstDate[1]);
+            int firstDay = int.Parse(splittedFirstDate[2]);
+
+            int secondYear = int.Parse(splittedSecondDate[0]);
+            int secondMonth = int.Parse(splittedSecondDate[1]);
+            int secondDay = int.Parse(splittedSecondDate[2]);
+
+            DateTime firstDateTime = new DateTime(firstYear, firstMonth, firstDay);
+            DateTime secondDateTime = new DateTime(secondYear, secondMonth, secondDay);
+
+            int daysBetween = int.Parse((secondDateTime - firstDateTime).TotalDays.ToString());
+
+            return Math.Abs(daysBetween).ToString();
+        }
+    }
+}
---
+++ Exercises/06_Defining Classes/Solutions/P05.DateModifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string firsDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier date = new DateModifier(firsDate, secondDate);
+
+            Console.WriteLine(date.DaysBetween());
+        }
+    }
+}
