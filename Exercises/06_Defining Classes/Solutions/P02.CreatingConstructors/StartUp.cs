728b80acb3676a9d51b9053c42002b02bac9c181
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-04 14:04:58 +0300

    Add P03. Oldest Family Member

--- Exercises/06_Defining Classes/Solutions/P02.CreatingConstructors/StartUp.cs
+++ Exercises/06_Defining Classes/Solutions/P02.CreatingConstructors/StartUp.cs
@@ -6,7 +6,18 @@
     {
         public static void Main()
         {
+            Person firstPerson = new Person();
+            Person secontPerson = new Person();
+            Person thirdPerson = new Person();
 
+            firstPerson.Name = "Pesho";
+            firstPerson.Age = 20;
+
+            secontPerson.Name = "Gosho";
+            secontPerson.Age = 18;
+
+            thirdPerson.Name = "Stamat";
+            thirdPerson.Age = 43;
         }
     }
 }
---
+++ Exercises/06_Defining Classes/Solutions/P03.OldestFamilyMember/Family.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Family
+    {
+        private List<Person> familyMember;
+
+        public Family()
+        {
+            familyMember = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            familyMember.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = familyMember
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+
+            return oldestMember;
+        }
+    }
+}
