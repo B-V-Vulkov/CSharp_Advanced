cab39e379b47400f4867f32619a9d1d89682cbb9
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 16:41:55 +0300

    Add P02. Generic Box of Integer

---
+++ Exercises/08_Generics/Solutions/P02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,33 @@
+﻿namespace P02.GenericBoxOfInteger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> boxCollection;
+
+        public Box()
+        {
+            boxCollection = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            boxCollection.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in boxCollection)
+            {
+                sb.AppendLine($"{item.GetType().FullName}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace P02.GenericBoxOfInteger
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<int> box = new Box<int>();
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= lines; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
