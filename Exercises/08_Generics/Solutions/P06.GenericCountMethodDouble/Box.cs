ec1158c0caecc7cf284d5ac50f50df953067768e
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 17:45:17 +0300

    Add P06. Generic Count Method Double

---
+++ Exercises/08_Generics/Solutions/P06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,53 @@
+﻿namespace P06.GenericCountMethodDouble
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> boxCollection;
+
+        public int CountGreater { get; private set; }
+
+        public Box()
+        {
+            boxCollection = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            boxCollection.Add(item);
+        }
+
+        public void Compare(T item)
+        {
+            foreach (var currentItem in boxCollection)
+            {
+                if (currentItem.CompareTo(item) > 0)
+                {
+                    CountGreater++;
+                }
+            }
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            T currentElement = boxCollection[index1];
+            boxCollection[index1] = boxCollection[index2];
+            boxCollection[index2] = currentElement;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in boxCollection)
+            {
+                stringBuilder.AppendLine($"{item.GetType().FullName}: {item}");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P06.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace P06.GenericCountMethodDouble
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<double> box = new Box<double>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lines; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                box.Add(element);
+            }
+
+            double stringToCopare = double.Parse(Console.ReadLine());
+            box.Compare(stringToCopare);
+
+            Console.WriteLine(box.CountGreater);
+        }
+    }
+}
