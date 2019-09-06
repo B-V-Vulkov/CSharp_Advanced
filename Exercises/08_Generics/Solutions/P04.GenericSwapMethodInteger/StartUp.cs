0a0fafc0435728b348b5f4f18a5434091a9d27f5
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 17:04:02 +0300

    Add P04. Generic Swap Method Integer

---
+++ Exercises/08_Generics/Solutions/P04.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace P04.GenericSwapMethodInteger
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<int> box = new Box<int>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lines; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                box.Add(element);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int index1 = indexes[0];
+            int index2 = indexes[1];
+
+            box.Swap(index1, index2);
+            Console.WriteLine(box);
+        }
+    }
+}
