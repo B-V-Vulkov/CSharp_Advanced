80642bac7da00d4e3f1f644581c3455eaa651944
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 16:23:38 +0300

    Add P01. Generic Box of String

---
+++ Exercises/08_Generics/Solutions/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace P01.GenericBoxOfString
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> boxCollection;
+
+        public Box()
+        {
+            boxCollection = new List<T>();
+        }
+
+        public void Add(T value)
+        {
+            boxCollection.Add(value);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in boxCollection)
+            {
+                sb.AppendLine($"{item.GetType().FullName}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace P01.GenericBoxOfString
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<string> box = new Box<string>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lines; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(box.ToString());
+
+        }
+    }
+}
