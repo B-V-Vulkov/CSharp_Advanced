d191e357ede7f13831e4b0088d6e7e8fede4358c
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 18:50:45 +0300

    Add P07. Tuple

---
+++ Exercises/08_Generics/Solutions/P07.Tuple/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace P07.Tuple
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] personBeerInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] numbersInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string personName = personInfo[0] + " " + personInfo[1];
+            string persnTown = string.Empty;
+
+            for (int i = 2; i < personInfo.Length; i++)
+            {
+                persnTown += personInfo[i] + " ";
+            }
+
+            string personBeerName = personBeerInfo[0];
+            double amountOfBeer = double.Parse(personBeerInfo[1]);
+
+            int myInteger = int.Parse(numbersInfo[0]);
+            double myDouble = double.Parse(numbersInfo[1]); 
+
+            Tuple<string, string> personTuple =
+                new Tuple<string, string>(personName, persnTown);
+
+            Tuple<string, double> personBeerTuple =
+                new Tuple<string, double>(personBeerName, amountOfBeer);
+
+            Tuple<int, double> numbersTuple =
+                new Tuple<int, double>(myInteger, myDouble);
+
+            Console.WriteLine(personTuple.GetInfo());
+            Console.WriteLine(personBeerTuple.GetInfo());
+            Console.WriteLine(numbersTuple.GetInfo());
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P07.Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿namespace P07.Tuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Tuple<T, K>
+    {
+        private T itam1;
+        private K itam2;
+
+        public Tuple(T itam1, K itam2)
+        {
+            this.itam1 = itam1;
+            this.itam2 = itam2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.itam1} -> {this.itam2}";
+        }
+    }
+}
