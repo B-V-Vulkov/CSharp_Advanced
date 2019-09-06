aa7879246b2a773348e8daa3343b22321361900f
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-06 17:40:18 +0300

    Add P05. Generic Count Method String

---
+++ Exercises/08_Generics/Solutions/P05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace P05.GenericCountMethodString
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<string> box = new Box<string>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string element = Console.ReadLine();
+                box.Add(element);
+            }
+
+            string stringToCopare = Console.ReadLine();
+            box.Compare(stringToCopare);
+
+            Console.WriteLine(box.CountGreater);
+        }
+    }
+}
