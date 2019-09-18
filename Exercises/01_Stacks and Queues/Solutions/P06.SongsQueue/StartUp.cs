6611d42177405b12388b9ba4984348bf5130b7c3
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-18 12:45:27 +0300

    Add P06.SongsQueue

---
+++ Exercises/01_Stacks and Queues/Solutions/P06.SongsQueue/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace P06.SongsQueue
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] sequenceOfSongs = Console.ReadLine()
+                .Split(", ");
+
+            Queue<string> songs = new Queue<string>(sequenceOfSongs);
+
+            while (songs.Count != 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command.StartsWith("Add"))
+                {
+                    string song = command.Substring(4);
+
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(song);
+                    }
+                }
+
+                else if (command.StartsWith("Play"))
+                {
+                    songs.Dequeue();
+                }
+
+                else if (command.StartsWith("Show"))
+                {
+                    Queue<string> currentSongs = new Queue<string>(songs);
+                    Console.WriteLine(string.Join(", ", currentSongs));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
