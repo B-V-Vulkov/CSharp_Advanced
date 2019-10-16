d98f19897fc573fd41d58538b85f3e61c844a4b8
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-16 15:13:27 +0300

    Add P08.Threeuple

---
+++ Exercises/08_Generics/Solutions/P08.Threeuple/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace P08.Threeuple
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ");
+
+            string[] personBeerInfo = Console.ReadLine()
+                .Split(" ");
+
+            string[] bankInfo = Console.ReadLine()
+                .Split(" ");
+
+            string firstPersonName = $"{personInfo[0]} {personInfo[1]}";
+            string firstPersonAddress = personInfo[2];
+            string firstPersonTown = personInfo[3];
+
+            bool seconPersonIsDrunk = false;
+
+            if (personBeerInfo[2] == "drunk")
+            {
+                seconPersonIsDrunk = true;
+            }
+
+            string secondePersonName = personBeerInfo[0];
+            double secondPersonLitersBeer = double.Parse(personBeerInfo[1]);
+
+            string thirdPersonName = bankInfo[0];
+            decimal thirdPersonAccount = decimal.Parse(bankInfo[1]);
+            string thirdPersonBankName = bankInfo[2];
+
+            var firstPersonThreeuple = 
+                new Threeuple<string, string, string>(firstPersonName, firstPersonAddress, firstPersonTown);
+
+            var secondPersonThreeuple =
+                new Threeuple<string, double, bool>(secondePersonName, secondPersonLitersBeer, seconPersonIsDrunk);
+
+            var thirdPersonThreeuple =
+                new Threeuple<string, decimal, string>(thirdPersonName, thirdPersonAccount, thirdPersonBankName);
+
+            Console.WriteLine(firstPersonThreeuple);
+            Console.WriteLine(secondPersonThreeuple);
+            Console.WriteLine($"{thirdPersonThreeuple.Item1} -> {thirdPersonThreeuple.Item2:f1} -> {thirdPersonThreeuple.Item3}");
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P08.Threeuple/Threeuple.cs
@@ -0,0 +1,23 @@
+﻿namespace P08.Threeuple
+{
+    public class Threeuple<T, K, L>
+    {
+        public Threeuple(T item1, K item2, L item3)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+        }
+
+        public T Item1 { get; set; }
+
+        public K Item2 { get; set; }
+
+        public L Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
