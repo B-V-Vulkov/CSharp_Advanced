fe24088e3a0e84571069886b108526d4ac9d669a
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-18 13:04:45 +0300

    Add P09.CustomLinkedList

---
+++ Exercises/08_Generics/Solutions/P09.CustomLinkedList/CustomList.cs
@@ -0,0 +1,224 @@
+﻿namespace CustomList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 2;
+
+        private T[] items;
+
+        public CustomList()
+        {
+            this.items = new T[InitialCapacity];
+            this.Count = 0;
+        }
+
+        public CustomList(int capacity)
+            : base()
+        {
+            this.items = new T[capacity];
+        }
+
+        public CustomList(IEnumerable<T> collection)
+        {
+            this.Count = collection.Count();
+            this.items = collection.ToArray();
+        }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return items[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                this.items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (this.items.Length == this.Count)
+            {
+                Resize();
+            }
+
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+
+        public T RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            T element = this.items[index];
+
+            this.Shift(index);
+
+            this.Count--;
+
+            this.Shrink();
+
+            return element;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = this.IndexOf(item);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            this.Shift(index);
+
+            this.Count--;
+
+            this.Shrink();
+
+            return true;
+        }
+
+        public void Clear()
+        {
+            this.items = new T[InitialCapacity];
+            this.Count = 0;
+        }
+
+        public int IndexOf(T item)
+        {
+            int index = -1;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+
+            if (this.items.Length == this.Count)
+            {
+                Resize();
+            }
+
+            for (int i = Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+
+            this.items[index] = item;
+            this.Count++;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            this.ValidateIndex(firstIndex);
+            this.ValidateIndex(secondIndex);
+
+            T currentElement = this.items[firstIndex];
+
+            this.items[firstIndex] = this.items[secondIndex];
+            this.items[secondIndex] = currentElement;
+        }
+
+        public bool Contains(T element)
+        {
+            if (this.Count == 0)
+            {
+                return false;
+            }
+
+            bool isContains = false;
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                if (this.items[i].Equals(element))
+                {
+                    isContains = true;
+                    break;
+                }
+            }
+
+            return isContains;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void Shrink()
+        {
+            if (this.Count * 4 >= this.items.Length)
+            {
+                return;
+            }
+
+            T[] copy = new T[this.items.Length / 2];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[Count - 1] = default(T);
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+    }
+}
---
+++ Exercises/08_Generics/Solutions/P09.CustomLinkedList/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Channels;
+
+namespace CustomList
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //Checking the constructors
+            var customList1 = new CustomList<int>();
+
+            var customList2 = new CustomList<string>(10);
+
+            var list = new List<char> {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+            var customList3 = new CustomList<char>(list);
+
+            //Checking Add method
+            customList1.Add(1);
+            customList2.Add("Text");
+            customList3.Add('J');
+
+            //Checking RemoveAt method
+            Console.WriteLine(customList1.RemoveAt(0)); //Return 1
+            //Console.WriteLine(customList1.RemoveAt(0));   //Return ArgumentOutOfRangeException
+
+            //Checking Remove method
+            Console.WriteLine(customList2.Remove("Text")); //Return True
+            Console.WriteLine(customList2.Remove("Text")); //Return False
+
+            //Checking IndexOf method
+            Console.WriteLine(customList3.IndexOf('A')); //Return 0
+            Console.WriteLine(customList3.IndexOf('Z')); //Return -1
+
+            //Checking Contains method
+            Console.WriteLine(customList3.Contains('A')); //Return True
+            Console.WriteLine(customList3.Contains('Z')); //Return False
+
+            //Checking Insert method
+            Console.WriteLine(string.Join(", ", customList3));  //Return A, B, C, D, E, F, G, H, I, J
+            customList3.Insert(3, 'Z');
+            Console.WriteLine(string.Join(", ", customList3));  //Return A, B, C, Z, D, E, F, G, H, I, J
+
+            //Checking Swap method
+            Console.WriteLine(string.Join(", ", customList3));  //Return A, B, C, Z, D, E, F, G, H, I, J
+            customList3.Swap(0, 1);
+            Console.WriteLine(string.Join(", ", customList3));  //Return B, A, C, Z, D, E, F, G, H, I, J
+
+            //Checking Clear method
+            customList3.Clear();
+            Console.WriteLine(customList3.Count); //Return 0
+        }
+    }
+}
