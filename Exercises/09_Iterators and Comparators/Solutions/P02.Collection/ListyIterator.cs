7e8775947f01fb7a442d59870f80131bc7fba330
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-22 15:03:44 +0300

    Add P02.Collection

---
+++ Exercises/09_Iterators and Comparators/Solutions/P02.Collection/ListyIterator.cs
@@ -0,0 +1,68 @@
+﻿namespace P02.Collection
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int index;
+
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            bool hasNext = this.HasNext();
+
+            if (hasNext)
+            {
+                this.index++;
+            }
+
+            return hasNext;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.elements[this.index]);
+        }
+
+        public void PrintAll() 
+        {
+            Console.WriteLine(string.Join(" ", this.elements));
+        }
+
+        public bool HasNext()
+        {
+            if (this.index < this.elements.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in this.elements)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
