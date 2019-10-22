066d61353de34a12231354becb5044f3ca616f6d
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-22 16:56:44 +0300

    Add P03.Stack

---
+++ Exercises/09_Iterators and Comparators/Solutions/P03.Stack/CustomStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace P03.Stack
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private const int initializedSize = 2;
+
+        private T[] innerArray;
+
+        private int pointer;
+
+        public CustomStack()
+        {
+            this.innerArray = new T[initializedSize];
+            this.pointer = -1;
+        }
+
+        public void Push(T element)
+        {
+            this.pointer++;
+
+            if (this.innerArray.Length == this.pointer)
+            {
+                this.IncreaseArraySize();
+            }
+
+            this.innerArray[pointer] = element;
+        }
+
+        public T Pop()
+        {
+            if (this.pointer < 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T result = innerArray[pointer];
+            this.pointer--;
+            return result;
+        }
+
+        private void IncreaseArraySize()
+        {
+            var copyArray = new T[this.innerArray.Length * 2];
+
+            for (int i = 0; i < this.pointer; i++)
+            {
+                copyArray[i] = this.innerArray[i];
+            }
+
+            this.innerArray = copyArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.pointer; i >= 0; i--)
+            {
+                yield return this.innerArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Exercises/09_Iterators and Comparators/Solutions/P03.Stack/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Stack
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var elements = Console.ReadLine()
+                .Split(',', ' ')
+                .Skip(1)
+                .ToArray();
+
+            var customStack = new CustomStack<string>();
+
+            foreach (var element in elements)
+            {
+                if (element != "")
+                {
+                    customStack.Push(element);
+                }
+            }
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string command = input.Split(' ')[0];
+
+                if (command == "Pop")
+                {
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch (InvalidOperationException ioex)
+                    {
+                        Console.WriteLine(ioex.Message);
+                    }
+                }
+                else if (command == "Push")
+                {
+                    customStack.Push(input.Split(' ')[1]);
+                }
+            }
+
+            foreach (var element in customStack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in customStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
