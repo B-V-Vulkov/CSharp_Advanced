1d88c487a3cb6339db7c54c41975b7d2a8ccfca1
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-22 17:16:52 +0300

    Add P04.Froggy

---
+++ Exercises/09_Iterators and Comparators/Solutions/P04.Froggy/Lake.cs
@@ -0,0 +1,45 @@
+﻿namespace P04.Froggy
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var evenPositions = new List<int>();
+            var oddPositions = new List<int>();
+
+            for (int i = 0; i < stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenPositions.Add(stones[i]);
+                }
+                else
+                {
+                    oddPositions.Add(stones[i]);
+                }
+            }
+
+            for (int i = 0; i < evenPositions.Count; i++)
+            {
+                yield return evenPositions[i];
+            }
+
+            for (int i = oddPositions.Count - 1; i >= 0; i--)
+            {
+                yield return oddPositions[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Exercises/09_Iterators and Comparators/Solutions/P04.Froggy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace P04.Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var stones = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+     
+        }
+    }
+}
