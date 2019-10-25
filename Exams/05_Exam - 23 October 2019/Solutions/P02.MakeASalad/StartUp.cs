7cee674b06180fa89c5fcf3bac6f57139a9e3c98
Borislav Valkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-25 18:36:04 +0300

    Add P02.MakeASalad

---
+++ Exams/05_Exam - 23 October 2019/Solutions/P02.MakeASalad/StartUp.cs
@@ -0,0 +1,70 @@
+﻿namespace P02.MakeASalad
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] inputVegetables = Console.ReadLine()
+                .Split(' ');
+
+            int[] inputCalories = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var vegetables = new Queue<string>(inputVegetables);
+            var calories = new Stack<int>(inputCalories);
+            var readySalads = new Queue<int>();
+
+            int currentCalories = 0;
+
+            while (vegetables.Count != 0 && calories.Count != 0)
+            {
+                int saladCalories = calories.Peek();
+                string vegetable = vegetables.Dequeue();
+
+                if (vegetable == "tomato")
+                {
+                    currentCalories += 80;
+                }
+                else if (vegetable == "carrot")
+                {
+                    currentCalories += 136;
+                }
+                else if (vegetable == "lettuce")
+                {
+                    currentCalories += 109;
+                }
+                else if (vegetable == "potato")
+                {
+                    currentCalories += 215;
+                }
+
+                if (currentCalories >= saladCalories)
+                {
+                    currentCalories = 0;
+                    readySalads.Enqueue(calories.Pop());
+                }
+                else if (calories.Count == 0 || vegetables.Count == 0)
+                {
+                    readySalads.Enqueue(saladCalories);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", readySalads));
+
+            if (vegetables.Count != 0)
+            {
+                Console.WriteLine(string.Join(" ", vegetables));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", calories));
+            }
+        }
+    }
+}
